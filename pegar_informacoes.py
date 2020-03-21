84b777736e600402a560a08661437dbc534ad417
igornascimentoflipe <igornascimentoflipe@penguin.lxd>
2020-03-20 21:22:38 -0300

    o programa agora consegue extrair a data da resposta no fórum

--- pegar_informacoes.py
+++ pegar_informacoes.py
@@ -1,14 +1,23 @@
-with open('saida.html','r') as arq:
+#le arquivo
+with open('teste.html','r') as arq:
     pagina = str(arq.readlines())
-#print(pagina)
+    
 import re
 
 #Resposta a t\xc3\xb3pico do f\xc3\xb3rum\n                            </td>\n                            <td #class="actions-table-actionDate">\n                                    7 horas atr\xc3\xa1s\n                            
 # </td>\n  
 
-#regex = 'Resposta{1}.{0,27}.{0,5}[0-5]rum{1}'
+#retira textos que começam com resposta e terminam com numeros
 regex = 'Resposta{1}.{0,100}rum.{0,100}.{0,100}.{0,100}[0-9]{1}'
-#print(re.search(regex,str(pagina)).group())
-print(len(re.findall(regex,str(pagina))))
-print(re.findall(regex,str(pagina))[70])
-#print(len(re.findall(regex,str(pagina))[0])-len('Resposta'))
+respostas = re.findall(regex,str(pagina))
+print(len(respostas))
+dados = ' '.join(respostas)
+#retira numero de dias ou horas ou semanas ou meses
+regex = '([0-9]*) (dias?|horas?|semanas?|m)'
+DHSM = re.findall(regex, dados)
+#retira datas no formato DD/MM/AAAA
+regex = '([0-9]{2}/[0-9]{2}/[0-9]{4})'
+data = re.findall(regex, dados)
+#imprime soma dos resultados para comparar com o numero de respostas e tambem imprime os valores
+print(len(data)+len(DHSM))
+print(DHSM+data)
--- principal.py
+++ principal.py
@@ -8,14 +8,14 @@ from bs4 import BeautifulSoup
 headers = {}
 
 with requests.Session() as s:
-    
+    #carrega cookie
     with open('cookie.txt','r') as arq:
         cookie = str(arq.readlines())
-        
+    #salva cookie no headers        
     headers['cookie'] = cookie    
-    
+    #faz req para site
     url = "https://cursos.alura.com.br/user/igor-nascimento-flipe/actions"
     r = s.get(url, headers=headers)
-    
+    #salva pagina em arquivo
     with open('teste.html','w') as arq:
         arq.writelines(str(r.content))
