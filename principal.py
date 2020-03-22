ad4f7cd203f2b3b0699489b672074a773e7c637b
igornascimentoflipe <igornascimentoflipe@penguin.lxd>
2020-03-21 22:15:27 -0300

    O program agora recebe o nome do usuario e salva as datas formatas em um arq .csv

--- principal.py
+++ principal.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 #https://www.youtube.com/watch?time_continue=598&v=fmf_y8zpOgA&feature=emb_logo
 #https://imasters.com.br/back-end/como-fazer-web-scraping-com-python
 
@@ -8,14 +9,27 @@ from bs4 import BeautifulSoup
 headers = {}
 
 with requests.Session() as s:
+    
     #carrega cookie
     with open('cookie.txt','r') as arq:
         cookie = str(arq.readlines())
+        
     #salva cookie no headers        
     headers['cookie'] = cookie    
+    
+    #receber nome do usuario
+    user = input("Nome do usuario: ")
+    
     #faz req para site
-    url = "https://cursos.alura.com.br/user/igor-nascimento-flipe/actions"
+    url = "https://cursos.alura.com.br/user/"+user+"/actions"
     r = s.get(url, headers=headers)
-    #salva pagina em arquivo
+    
+    #pega data e hora para colocar no topo do arquivo
+    from datetime import datetime
+    data_atual = datetime.now()
+    
+    #salva pagina em arquivo com data e hora do registro
     with open('teste.html','w') as arq:
+        arq.writelines(str(data_atual))
+        arq.writelines('\n')
         arq.writelines(str(r.content))
